3920484c834f5aaa97ed1015b1dd536a9efd76e7
Vladimir_Medyannikov <78690907+vmedyannikov76@users.noreply.github.com>
2024-01-24 10:45:38 +0300

    перебор
    формирование нового объекта путем перебора исходного.
    Есть ли решение проще? правильнее?

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,9 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  const newObject = {};
+  for (let i in obj) {
+    newObject[obj[i]] = i;
+  }
+  return obj ? newObject : obj
 }

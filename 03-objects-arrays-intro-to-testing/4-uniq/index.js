106441f1eab30dad01f67c5bcdaa4f26ccc48515
medya <vmedyannikov76@gmail.com>
2024-01-26 21:59:27 +0300

    3-4

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,13 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (!arr) {return [];}
+  if (!arr.length) return [];
+  const newArr = new Set();
 
+  arr.forEach(element => {
+    newArr.add(element);
+  });
+  return Array.from(newArr)
 }
+

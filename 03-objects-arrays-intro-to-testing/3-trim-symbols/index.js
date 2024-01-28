477c39f4d5ba720f531a40125c5005847b5eb97a
Vladimir_Medyannikov <78690907+vmedyannikov76@users.noreply.github.com>
2024-01-28 10:22:22 +0300

    3-3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,15 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  let newStr = '';
+  let count = 1;
+  for (let i = 0; i < string.length; i++) {
+    let test = string[i];
+    newStr.at(-1) === test ? count++ : count = 1;
+    if (count > size) {
+      continue;
+    }
+    newStr += string[i];
+  }
+  return newStr;
 }
